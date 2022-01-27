664b60ece2530b5eba629e43422f3e173bf2b727
sandip0721 <rathod2sandip1997@gmail.com>
2022-01-27 15:24:42 +0530

    workshop#000 completed

---
+++ script.js
@@ -0,0 +1,12 @@
+var form = document.getElementById('form')
+form.addEventListener('submit',function(event){
+    event.preventDefault()
+    var username = document.getElementById('username').value
+    console.log(username)
+
+    var email = document.getElementById('email').value
+    console.log(email)
+
+    var password = document.getElementById('password').value
+    console.log(password)
+})
